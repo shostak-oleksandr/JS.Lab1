0e29a20bdb985cd8c547fd061e5caf449924a896
shostak-oleksandr <85931950+shostak-oleksandr@users.noreply.github.com>
2022-02-11 18:44:53 +0200

    Update
    Installed module mocha. Written tests Jest and SuperTest. Created file .env to change the appearance of the answer.

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const express = require("express");
 // створюємо об’єкт додатка
 const app = express();
 
+function jest_test(phrase) {
+    return phrase + "!";
+}
+
+module.exports = {
+    jest_test
+  }
+
 // визначаємо обробник для маршруту "/"
 app.get("/", (request, response) => {
     
     // відправляємо відповідь
     response.send("<h1>Hello World!</h1>");
+
+    jest_test();
 });
 
 // починаємо прослуховувати підключення на 3000 порту
